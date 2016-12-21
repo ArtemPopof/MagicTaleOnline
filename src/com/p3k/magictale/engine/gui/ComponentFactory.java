678e4ac34e8d4637dab67b90d331fd78557c3b19
Георгий Попов <popoff96@live.com>
2016-12-21 12:26:20 +0300

    Change factories to support new elements  - Action bar  - Status bar  - Player menu

--- src/com/p3k/magictale/engine/gui/ComponentFactory.java
+++ src/com/p3k/magictale/engine/gui/ComponentFactory.java
@@ -1,5 +1,8 @@
 package com.p3k.magictale.engine.gui;
 
+import com.p3k.magictale.engine.graphics.Sprite;
+import com.p3k.magictale.game.Characters.Player;
+
 /**
  * Created by jorgen on 14.12.16.
  */
@@ -7,9 +10,15 @@ public abstract class ComponentFactory {
 
     protected String RES_PATH = "";
 
-    public abstract MButton createButton(String text, float x, float y);
-    //public abstract MWindow createWindow();
+    public abstract Button createButton(String text, float x, float y);
 
-    // TODO more components
-    public abstract  MText createText(String text);
+    public abstract Button createButtonSmall(Sprite image, float x, float y);
+
+    public abstract Text createText(String text);
+
+    public abstract StatusBar createStatusBar(Player player);
+
+    public abstract ActionBar createActionBar(Player player);
+
+    public abstract PlayerMenu createPlayerMenu();
 }
