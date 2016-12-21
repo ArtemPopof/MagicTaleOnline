a80455c3b940cc30f57945a34f876139cd6abb0b
Aleksandr Ploskov <merhalak@gmail.com>
2016-12-21 07:26:21 +0300

    Change GameObjects to threadsafe

--- src/com/p3k/magictale/game/GameObjects.java
+++ src/com/p3k/magictale/game/GameObjects.java
@@ -2,36 +2,39 @@ package com.p3k.magictale.game;
 
 import com.p3k.magictale.engine.graphics.GameObject;
 
-import java.util.ArrayList;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * For server syncronization
- *
+ * <p>
  * Created by artem96 on 20.12.16.
  */
-public class GameObjects {
+public class GameObjects extends UnicastRemoteObject {
 
-    ArrayList<GameObject> objects;
+    private final ConcurrentHashMap<Integer, GameObject> objects;
 
-    public GameObjects() {
-        objects = new ArrayList<>();
+    public GameObjects() throws RemoteException {
+        objects = new ConcurrentHashMap<>();
     }
 
-    public GameObject get(int index) {
+    public GameObject get(int index) throws RemoteException {
         return objects.get(index);
     }
 
-    public void set(GameObject object, int index) {
-        this.objects.set(index, object);
+    public void set(GameObject object, int index) throws RemoteException {
+        this.objects.put(index, object);
     }
 
-    public int size() {
+    public int size() throws RemoteException {
         return objects.size();
     }
 
-    public void add(GameObject object) {
-        objects.add(object);
+    public void add(GameObject object) throws RemoteException {
+        synchronized (objects) {
+            objects.put(objects.size(), object);
+        }
     }
 
 
