94d661f3f4b8c24a101bc80617e524389c57eca7
GeorgePopoff <popoff96@live.com>
2017-01-16 17:28:50 +0300

    Added client player state class

---
+++ src/client/Player.java
@@ -0,0 +1,116 @@
+package client;
+
+import com.p3k.magictale.game.Characters.CharacterTypes;
+
+/**
+ * Created by jorgen on 16.01.17.
+ */
+public class Player {
+    enum MoveDirection {
+        NONE,
+        MOVE_RIGHT,
+        MOVE_LEFT,
+        MOVE_UP,
+        MOVE_DOWN
+    }
+
+    private int currentState;
+    private int currentHealth;
+    private int maxhealth;
+    private MoveDirection moveDirection;
+    private CharacterTypes CharacterType;
+    private float speed;
+    private int attack;
+    private boolean isDead;
+    private int currentLevel;
+    private int gainedExperience;
+    private int xp;
+
+    public int getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(int currentState) {
+        this.currentState = currentState;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public int getMaxhealth() {
+        return maxhealth;
+    }
+
+    public void setMaxhealth(int maxhealth) {
+        this.maxhealth = maxhealth;
+    }
+
+    public MoveDirection getMoveDirection() {
+        return moveDirection;
+    }
+
+    public void setMoveDirection(MoveDirection moveDirection) {
+        this.moveDirection = moveDirection;
+    }
+
+    public CharacterTypes getCharacterType() {
+        return CharacterType;
+    }
+
+    public void setCharacterType(CharacterTypes characterType) {
+        CharacterType = characterType;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public void setDead(boolean dead) {
+        isDead = dead;
+    }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public int getGainedExperience() {
+        return gainedExperience;
+    }
+
+    public void setGainedExperience(int gainedExperience) {
+        this.gainedExperience = gainedExperience;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+}
